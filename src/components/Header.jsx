27ef26a35d62645e57656730e372fa6772e52c1e
ystk <kaihatsu20190316@gmail.com>
2024-08-13 16:46:37 +0900

    ベース機能追加

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,32 @@
-import CoverImage from '../images/cover-image.jpg'
-import ProfileImage from '../images/profile-image.png'
+import CoverImage from '../images/cover-image.jpg';
+import ProfileImage from '../images/profile-image.png';
+import { FaTwitter, FaGithub } from 'react-icons/fa';
 
 export const Header = () => {
-  const componentName = () => 'Header';
-
   return (
-    <div className="test-wrapper">
-      <p className="text">
-        このコンポーネントの名前は{componentName()}です。
-      </p>
-    </div>
+    <header className="main-cover" style={{ backgroundImage: `url(${CoverImage})` }}>
+      <div className="overlay"></div>
+      <div className="container">
+        <div className="display-table">
+          <div className="display-table-contents">
+            <div className="profile-thumb" style={{ backgroundImage: `url(${ProfileImage})` }}></div>
+            <h1 className="title-text">トゲピー</h1>
+            <h3 className="title-text">SE</h3>
+            <ul className="social-icons">
+              <li className="icon-link">
+                <a href="https://twiiter.com/">
+                  <FaTwitter color='white' size="2rem" />
+                </a>
+              </li>
+              <li className="icon-link">
+                <a href="https://github.com/">
+                  <FaGithub color='white' size="2rem" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
